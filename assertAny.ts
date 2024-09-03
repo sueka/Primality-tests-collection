27876f3c6f9d87162eaed60689dc343211ac3023
sueka <mail@sueka.me>
2024-09-04 00:29:01 +0900

    :test_tube: Add assertAny for disjunctive assertion

---
+++ assertAny.ts
@@ -0,0 +1,26 @@
+import { AssertionError } from "@std/assert";
+
+/**
+ * Asserts some of assertions will succeed.
+ */
+export default function assertAny(assertions: (() => void)[]): void {
+  let assertionError: AssertionError
+
+  const passed = assertions.some(assertion => {
+    try {
+      assertion()
+
+      return true
+    } catch (error: unknown) {
+      if (error instanceof AssertionError) {
+        assertionError = error
+      }
+
+      return false
+    }
+  })
+
+  if (!passed) {
+    throw assertionError!
+  }
+}
